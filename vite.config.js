649de2b963b4cd02377c8e52bd3047030fa245ec
Kazutoshi Tanaka <kazutoshi.tanaka@gmail.com>
2025-09-22 11:29:08 +0900

    base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/prc-arm-pose/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
